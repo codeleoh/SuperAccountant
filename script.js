cb9db23142d0b01b9b7791080b7798c4eb4ae3e8
Leonardo Henrique <code.leoh@gmail.com>
2023-05-23 12:05:59 -0300

    Concluindo o exercício

--- script.js
+++ script.js
@@ -2,10 +2,32 @@ function contar() {
     let inicio = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
     
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        alert('[ERRO] Faltam dados!!!')
+        res.innerHTML = 'Impossível contar!'
     } else {
-        alert('Tudo Ok!')
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if (p <= 0 ) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+
+        if (i < f) {
+        // Contagem Crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+        // Contagem Regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += '\u{1F3C1}'
     }
 }
